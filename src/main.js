b42cd36db1115ca9605f1957a6f92e49edaa797f
sieterne <siebe.ternest@gmail.com>
2026-03-14 01:41:10 +0100

    Fly Mode

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createScene } from "./viewer/scene.js";
-import { createCamera, fitCameraToObject } from "./viewer/camera.js";
-import { createRenderer } from "./viewer/renderer.js";
+import * as THREE from "three";
 
+import { createScene } from "./viewer/scene.js";
+import { createCamera, fitCameraToObject, FlyControls } from "./viewer/camera.js";
+import { createRenderer } from "./viewer/renderer.js";
 import { loadPointCloud } from "./loaders/pcdLoader.js";
 
 const scene = createScene();
 const camera = createCamera();
 const renderer = createRenderer();
+const flyControls = new FlyControls(camera, renderer.domElement);
+const clock = new THREE.Clock();
 const datasetSelect = document.getElementById("dataset");
 
 let currentPointCloud = null;
@@ -61,6 +64,9 @@ loadSelectedDataset();
 function animate() {
 
   requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+  flyControls.update(delta);
 
   renderer.render(scene, camera);
 
